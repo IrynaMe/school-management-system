a41067b7dea732146dcb7950b719a810612d1a8b
IrynaMe <irinam7a@gmail.com>
2024-04-23 10:01:30 +0200

    new commit

--- src/com/garderie/Main.java
+++ src/com/garderie/Main.java
@@ -2,6 +2,7 @@ package com.garderie;
 
 public class Main {
     public static void main(String[] args) {
+        //comment to test
         Menu menu = new Menu();
         menu.mainLoop();
     }
